6400ef36a8eabd8a292a018495120cb659e86dd5
oishikm <oishik.mukhopadhyay@gmail.com>
2023-09-03 13:09:04 +0530

    Added gpu mode options

--- select-gpu.c
+++ select-gpu.c
@@ -14,14 +14,39 @@ void getCommand(char* command, char* option)
     }
     else
     {
+        if(strcmp(option, "h") == 0) /* hybrid */
+        {
+            strcpy(option, "hybrid");
+        }
+        if(strcmp(option, "i") == 0) /* integrated */
+        {
+            strcpy(option, "integrated");
+        }
+        if(strcmp(option, "n") == 0 || strcmp(option, "d") == 0) /* nvidia - dedicated */
+        {
+            strcpy(option, "nvidia");
+        }        
+        if(strcmp(option, "c") == 0) /* compute */
+        {
+            strcpy(option, "compute");
+        }
         sprintf(command, "sudo system76-power graphics %s", option);   
     }
 }
 
-int main()
+int main(int argc, char** argv)
 {
     char command[256];
-    char current_gpu[128];        
+    char current_gpu[128];
+    char option[128];
+    if(argc > 1)
+    {
+        strcpy(option, argv[1]);
+    }
+    else
+    {
+        strcpy(option, "default");
+    }
     getCommand(command, "help");
     printf("%s", command);
     FILE *fp = popen("sudo system76-power graphics", "r");
